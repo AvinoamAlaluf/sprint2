ad318cc3a89ee3ed8cec6b6db05658e8922aaaa2
larisa shousterman <lshousterman@gmail.com>
2017-11-16 19:30:25 +0200

    data stractuer

---
+++ js/app.js
@@ -0,0 +1,48 @@
+var gImgs = [{
+        id: 1,
+        url: 'img/gallery/danAsWorier.jpg',
+        text: "dan as worier",
+        keywords: ['strong']
+    },
+    {
+        id: 2,
+        url: 'img/gallery/danWithAHat.jpg',
+        text: "dan With A Hat",
+        keywords: ['peaceful']
+    },
+    {
+        id: 3,
+        url: 'img/gallery/danWithBear.jpg',
+        text: "dan With A Hat keywords",
+        keywords: ['satisfied']
+    },
+    {
+        id: 4,
+        url: 'img/yaronAndAsafWithSpoons',
+        text: "yaron And Asaf With Spoons",
+        keywords: ['happy']
+    },
+    {
+        id: 5,
+        url: 'img/yaronSurprised',
+        text: "yaron Surprised",
+        keywords: ['Surprised']
+    },
+    {
+        id: 6,
+        url: 'img/yaronWithPhone',
+        text: "yaron With Phone",
+        keywords: ['tech oriented']
+    }
+];
+
+
+var gMeme = {
+    selectedImgId: 5,
+    txts: [{
+        line: 'I never eat Falafel',
+        size: 20,
+        align: 'left',
+        color: 'red'
+    }]
+}
