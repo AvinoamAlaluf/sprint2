4b783a7270d0e13e11600091cab0c4935e0f6d71
Avinoam Alaluf <avinoamalaluf@gmail.com>
2017-11-19 14:56:06 +0200

    added color pallete to meme Editor

--- js/htmlChanges.js
+++ js/htmlChanges.js
@@ -6,3 +6,12 @@ function toggleMemeEditorPopup(){
         elMemeEditorPopup.classList.add('hidden');
     }
 }
+
+function toggleColorEditorPopup(){
+    var elMemeEditorPopup = document.querySelector('.colorEditorPopup');
+    if (elMemeEditorPopup.classList.contains('hidden')) {
+        elMemeEditorPopup.classList.remove('hidden');
+    }else{
+        elMemeEditorPopup.classList.add('hidden');
+    }
+}
